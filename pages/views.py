7c236ab44b810aa437684ec3a3364d646ba94fa7
chesspro13 <brandon@mauldin314.com>
2021-02-13 18:34:05 -0700

    Testing downloader

--- pages/views.py
+++ pages/views.py
@@ -131,7 +131,7 @@ def downloadAudio( request, *args, **kwargs ):
     f= open("/home/pi/commands/downloadThis", 'w')
     #f.write( str(url[2:-1]))
     f.close()
-    print("Downloading audio for " + str(url ))
+    print("Downloading audio for " + str(url[2:-1] ))
     #subprocess.run(['youtube-dl', '--extract-audio', '--audio-format', 'mp3', '-o', '/home/pi/songs/%(title)s.%(ext)s', url])
     #subprocess.run(['rm', '/home/pi/songs/%(title)s.webm'])
     return HttpResponse("Copy")
